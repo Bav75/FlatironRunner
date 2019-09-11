cae779d3bc3c228b5b1e674b023cd6b109afc178
Brian Velez <bvez94@gmail.com>
2019-09-11 10:27:00 -0700

    revised routes for games and players resources.

--- flatiron-runner-backend/db/migrate/20190911160632_create_players.rb
+++ flatiron-runner-backend/db/migrate/20190911160632_create_players.rb
@@ -2,6 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[5.2]
   def change
     create_table :players do |t|
       t.string :username
+      # store player progress in an array and run checks against it
       t.text :progress, array: true, default: [].to_yaml
       t.string :character
 
