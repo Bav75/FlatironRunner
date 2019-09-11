027700384671dd5ee7ce03640d28dd4e18d978c8
Brian Velez <bvez94@gmail.com>
2019-09-11 10:40:48 -0700

    added show for Game. beginning to implement game page

--- flatiron-runner-backend/config/routes.rb
+++ flatiron-runner-backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :games, only: [:create, :show, :update]
+  resources :games, only: [:show, :update]
   resources :players, only: [:create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
