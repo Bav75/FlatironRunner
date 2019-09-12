67e3a806d492059228221e1995c9a3c75170eb02
Brian Velez <bvez94@gmail.com>
2019-09-12 10:46:51 -0700

    added first game states, title & menu

--- flatiron-runner-frontend/src/index.js
+++ flatiron-runner-frontend/src/index.js
@@ -21,6 +21,8 @@ let ctx = cvs.getContext("2d");
 // load all image assets 
 let bg = new Image();
 bg.src = "assets/purple_bg.jpeg";
+let title = new Image();
+title.src = "assets/titleScreen.png";
 
 // initialize the game session
 let masterGame = new Game("title");
@@ -30,14 +32,45 @@ function draw() {
     // // I have to give the img time to load.
     // // remember ASYNC! 
 
-    bg.onload = function() {
+    title.onload = function() {
         // let masterGame = new Game("title");
-        ctx.drawImage(bg, 0, 0, 900, 512); 
+        
+        ctx.drawImage(title, 0, 0, 800, 512);
+
+        // if player is on start screen & clicks then change state to menu  
+        // handle the state check here instead of a sep function? 
+
+        // setTimeout(function() {
+        //     if (masterGame.state === "title") {
+        //         cvs.onclick = function() {
+        //             changeState(masterGame.state);}
+        //         ctx.drawImage(bg, 0, 0, 800, 512);
+        //     };
+        // }, 5000);
+
+        // event was firing autoamtically because I was passing value instead of reference to function 
+        // cvs.addEventListener('click', function() {
+        //     changeState(masterGame.state)}
+        //     );
+
+        cvs.onclick = function() {
+            changeState(masterGame.state);
+        };
+       
+        // masterGame.addEventListener("keydown", changeState);
+
     }
 
     // bg.src = "assets/purple_bg.jpeg";
     // ctx.drawImage(bg, 0, 0, 900, 512);
 };
+
+function changeState(gameState) {
+    if (gameState === "title") {
+        masterGame.state = "menu";
+        ctx.drawImage(bg, 0, 0, 800, 512);
+    };
+}
 
 draw();
 
