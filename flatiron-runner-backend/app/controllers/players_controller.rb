5c53c330a4f0af183468a2f806e7cf4d7caa843b
Brian Velez <bvez94@gmail.com>
2019-09-12 12:56:45 -0700

    added find/create logic to players#create. created serialzers dir.

--- flatiron-runner-backend/app/controllers/players_controller.rb
+++ flatiron-runner-backend/app/controllers/players_controller.rb
@@ -2,6 +2,12 @@ class PlayersController < ApplicationController
 
     def create
         binding.pry
+
+        username = params[:username]
+
+        player = Player.find_or_create_by(username: username)
+
+        binding.pry
     end
 
 end
