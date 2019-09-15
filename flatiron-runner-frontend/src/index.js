58eb4561e04c545807f1dc34d27f136cc0be0101
Brian Velez <bvez94@gmail.com>
2019-09-15 10:39:08 -0700

    added collision detection

--- flatiron-runner-frontend/src/index.js
+++ flatiron-runner-frontend/src/index.js
@@ -95,7 +95,6 @@ title.onload = function() {
 // FOR LOOP APPROACH 
 let obstacles = [];
 obstacles[0] = {x: cvs.width, y: 380};
-// obstacles.push({x: cvs.width, y: 380});
 
 
 // WHILE LOOP APPROACH
@@ -112,21 +111,19 @@ obstacles[0] = {x: cvs.width, y: 380};
 var reloadCounter = 0;
 
 document.addEventListener("keydown", function(e) {
-    // handle jumping using space bar 
     if (sX < 705 && sY > -100) {
+        // handle jumping using space bar 
         if (e.keyCode === 32) {
             jump();
         };
     
-        // handle moving forward
+        // handle moving forward using d
         if (e.keyCode === 68) {
             moveRight();
         };
-    
-        
     };
 
-    // handle moving backwards
+    // handle moving backwards using a
     if (sX > -1) {
         if (e.keyCode === 65) {
             moveLeft();
@@ -140,21 +137,9 @@ function draw() {
     // var bg = new Image();
     // // I have to give the img time to load.
     // // remember ASYNC! 
+
     if (masterGame.state === "bg") {
-        // let obstacles = [];
-        // obstacles[0] = {x: cvs.width, y: 380};
-        // obstacles[0] = {x: 500, y: 380};
-
-        // ctx.drawImage(bg, 0, 0, 800, 512);
-        // ctx.drawImage(sprite, sX, sY, 165, 270); 
         // x = 0, y = 200 is the ground for the man sprite 
-            
-        // ctx.drawImage(bomb, cvs.width - 600, 380, 70, 70);
-
-        // while (obstacles[0].x > 0) {
-        //     obstacles[0].x--;
-        // };
-
 
         // ********************************************************
         // FOR LOOP APPROACH START 
@@ -168,26 +153,28 @@ function draw() {
                 location.reload();
             } else {
                 // console.log(reloadCounter);
-                ctx.drawImage(bomb, obstacles[i].x, 380, 70, 70);
+                ctx.drawImage(bomb, obstacles[i].x, obstacles[i].y, 70, 70);
                 obstacles[i].x -= 7.5;
-    
-                 if (obstacles[i].x == cvs.width - 900) {
-                     obstacles.push({
-                         x: cvs.width,
-                         y: 380
-                     });
-                     ++reloadCounter;
+
+                // collisions
+
+                if (sX + 11 >= obstacles[i].x && sX <= obstacles[i].x + 70 && sY + 180  >= obstacles[i].y) {
+                    // sX + 11 >= obstacles[i].x || sX <= obstacles[i].x + 70 && sY + 22  >= obstacles[i].y
+                    // (sY <= obstacles[i].y + 30 || sY + 22  >= obstacles[i].y)
+                    // sY + 180  >= obstacles[i].y)
+                    alert("You hit the bomb!");
+                    // alert(`bomb height ${obstacles[i].y}, sprite height ${sprite.height}, sY ${sY}`);
+                    location.reload();
+                };
+                
+                if (obstacles[i].x == cvs.width - 900) {
+                    obstacles.push({
+                        x: cvs.width,
+                        y: 380
+                    });
+                    ++reloadCounter;
                 };
             };
-            // ctx.drawImage(bomb, obstacles[i].x, 380, 70, 70);
-            // obstacles[i].x--;
-
-            //  if (obstacles[i].x == cvs.width - 600) {
-            //      obstacles.push({
-            //          x: cvs.width,
-            //          y: 380
-            //      });
-            // };
 
             // player sprite fall speed
             if (sY <= 200) {
@@ -196,76 +183,11 @@ function draw() {
         };
         // ********************************************************
         // FOR LOOP APPROACH END 
-
-        // ********************************************************
-        // WHILE LOOP APPROACH START
-        // ctx.drawImage(bomb, obstacles[0].x, 380, 70, 70);
-        // ctx.drawImage(bomb, obstacle.x, obstacle.y, 70, 70);
-        // // let i = 0;
-        // let i = [];
-        // while (i.length < 5) {
-        //     console.log(i);
-        //     // ctx.drawImage(bomb, obstacles[0].x, 380, 70, 70);
-        //     // console.log(i);
-
-        //     // obstacles[0].x--;
-        //     obstacle.x--;
-        //     // --obstacle.x;
-
-        //     // if (obstacles[0].x == cvs.width - 600) {
-        //     if (obstacle.x == cvs.width - 600) {
-        //         console.log("You hit the inside")
-        //         // obstacles.push({
-        //         //     x: cvs.width,
-        //         //     y: 380
-        //         // });
-        //         obstacle.x = cvs.width;
-        //         // i += 1;
-        //         i.push(1);
-        //     };
-
-        // WHILE LOOP APPROACH END 
-        // ********************************************************
-        };
-        // for (x = 0; x < DIFFICULTY; x++) {
-        //     ctx.drawImage(bomb, (200 + GAP + (Math.floor(Math.random() * 500))), 380, 70, 70); 
-        // };
-
-        // ctx.drawImage(bomb, 200, 380, 70, 70); 
-        // x = 200, y = 380 is the ground for the bomb sprite 
-        requestAnimationFrame(draw);
     };
-    
-    
-        // let masterGame = new Game("title");
         
-
-        // if player is on start screen & clicks then change state to menu  
-        // handle the state check here instead of a sep function? 
-
-        // setTimeout(function() {
-        //     if (masterGame.state === "title") {
-        //         cvs.onclick = function() {
-        //             changeState(masterGame.state);}
-        //         ctx.drawImage(bg, 0, 0, 800, 512);
-        //     };
-        // }, 5000);
-
-        // event was firing autoamtically because I was passing value instead of reference to function 
-        // cvs.addEventListener('click', function() {
-        //     changeState(masterGame.state)}
-        //     );
-
-     
-       
-        // masterGame.addEventListener("keydown", changeState);
-
-
-    // requestAnimationFrame(draw);
-
-    // bg.src = "assets/purple_bg.jpeg";
-    // ctx.drawImage(bg, 0, 0, 900, 512);
-// };
+    requestAnimationFrame(draw);
+};
+    
 
 function changeState(gameState) {
     // if (gameState === "title") {
@@ -309,7 +231,74 @@ function moveLeft() {
 // update player model progress - have it be an integer that points to a specific set of levels on the frontend 
 
 draw();
+
+
+  // ********************************************************
+        // WHILE LOOP APPROACH START
+        // ctx.drawImage(bomb, obstacles[0].x, 380, 70, 70);
+        // ctx.drawImage(bomb, obstacle.x, obstacle.y, 70, 70);
+        // // let i = 0;
+        // let i = [];
+        // while (i.length < 5) {
+        //     console.log(i);
+        //     // ctx.drawImage(bomb, obstacles[0].x, 380, 70, 70);
+        //     // console.log(i);
+
+        //     // obstacles[0].x--;
+        //     obstacle.x--;
+        //     // --obstacle.x;
+
+        //     // if (obstacles[0].x == cvs.width - 600) {
+        //     if (obstacle.x == cvs.width - 600) {
+        //         console.log("You hit the inside")
+        //         // obstacles.push({
+        //         //     x: cvs.width,
+        //         //     y: 380
+        //         // });
+        //         obstacle.x = cvs.width;
+        //         // i += 1;
+        //         i.push(1);
+        //     };
+
+        // WHILE LOOP APPROACH END 
+        // ********************************************************
+
+// for (x = 0; x < DIFFICULTY; x++) {
+        //     ctx.drawImage(bomb, (200 + GAP + (Math.floor(Math.random() * 500))), 380, 70, 70); 
+        // };
+
+        // ctx.drawImage(bomb, 200, 380, 70, 70); 
+        // x = 200, y = 380 is the ground for the bomb sprite 
+
+
 // requestAnimationFrame(draw);
 
+        // let masterGame = new Game("title");
+        
+
+        // if player is on start screen & clicks then change state to menu  
+        // handle the state check here instead of a sep function? 
+
+        // setTimeout(function() {
+        //     if (masterGame.state === "title") {
+        //         cvs.onclick = function() {
+        //             changeState(masterGame.state);}
+        //         ctx.drawImage(bg, 0, 0, 800, 512);
+        //     };
+        // }, 5000);
+
+        // event was firing autoamtically because I was passing value instead of reference to function 
+        // cvs.addEventListener('click', function() {
+        //     changeState(masterGame.state)}
+        //     );
+
+     
+       
+        // masterGame.addEventListener("keydown", changeState);
 
 
+    // requestAnimationFrame(draw);
+
+    // bg.src = "assets/purple_bg.jpeg";
+    // ctx.drawImage(bg, 0, 0, 900, 512);
+// };
